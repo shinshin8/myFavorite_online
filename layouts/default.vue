70ac57eb249205ba1a7b28881f4fb0cbfa903303
sax.s.kanno.g <s.kanno.sax@gmail.com>
2019-06-10 23:10:41 +0900

    Created Post List

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app dark>
+  <v-app>
     <v-navigation-drawer
       v-model="drawer"
       :mini-variant="miniVariant"
@@ -84,7 +84,7 @@ export default {
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'Find My Favorite'
     }
   }
 }
