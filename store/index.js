247ff17519b2f9eef87d3fbf4a47c7608d0c1ffb
sax.s.kanno.g <s.kanno.sax@gmail.com>
2019-06-29 22:01:37 +0900

    Made Sign Up

--- store/index.js
+++ store/index.js
@@ -22,8 +22,46 @@ export const actions = {
         username,
         password
       })
-      const user = res.token
-      commit('setUser', { user })
+      if (res.status && res.error_code === 0) {
+        const user = res.token
+        commit('setUser', { user })
+      } else {
+        throw new Error('Username or password is invalid.')
+      }
+    } catch (error) {
+      if (error.response && error.response.status === 401) {
+        throw new Error('Bad credentials')
+      }
+      throw error
+    }
+  },
+  async signUp(
+    { commit },
+    { username, emailAddress, password, confirmPassword }
+  ) {
+    try {
+      const res = await this.$axios.$post('/signUp', {
+        username,
+        emailAddress,
+        password,
+        confirmPassword
+      })
+      if (!res.status && res.error_code === 3) {
+        throw new Error('Invalid Username')
+      }
+      if (!res.status && res.error_code === 4) {
+        throw new Error('Invalid mail address')
+      }
+      if (!res.status && res.error_code === 5) {
+        throw new Error('Invalid password')
+      }
+      if (!res.status && res.error_code === 6) {
+        throw new Error('Password does not match with comfirmed password  ')
+      }
+      if (res.status && res.error_code === 0) {
+        const user = res.token
+        commit('setUser', { user })
+      }
     } catch (error) {
       if (error.response && error.response.status === 401) {
         throw new Error('Bad credentials')
